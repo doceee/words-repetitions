fcef4160f2f161906df8577919dc3f4e33412463
doceee <piotr96X@wp.pl>
2024-08-22 13:58:47 +0200

    init

---
+++ landing-page/components/TopBar.vue
@@ -0,0 +1,21 @@
+<template>
+    <nav class="w-full bg-primary">
+        <div
+            class="mx-auto flex w-full max-w-7xl items-center justify-between px-[18px] py-[10px] text-center"
+        >
+            <p
+                class="relative inline-block font-rowdies font-extrabold before:absolute before:-inset-1 before:block before:-skew-y-2 before:bg-orange-400"
+            >
+                <span class="relative text-white"> Vocab<br />Master </span>
+            </p>
+
+            <a
+                target="_blank"
+                href="https://www.app.vocabmaster.pl"
+                class="flex h-fit justify-center rounded-[10px] border-[2px] border-white bg-primary px-[8px] py-[4px] text-[13px] font-bold text-white hover:bg-white hover:text-primary"
+            >
+                Rozpocznij naukę
+            </a>
+        </div>
+    </nav>
+</template>
---
+++ landing-page/components/WhyUs.vue
@@ -0,0 +1,42 @@
+<template>
+    <section
+        class="mx-auto my-[20px] w-full max-w-4xl items-center justify-center px-[12px] py-[60px] text-center"
+    >
+        <header>
+            <h1 class="text-[28px] font-bold leading-[38px] text-gray-800">
+                Dlaczego
+                <span
+                    class="relative inline-block before:absolute before:-inset-1 before:block before:-skew-y-2 before:bg-orange-400"
+                >
+                    <span class="relative text-white">VocabMaster</span>
+                </span>
+                ?
+            </h1>
+        </header>
+        <div class="mt-[38px] flex">
+            <div v-for="item in data" :key="item.header">
+                <p class="text-[14px] font-[600]">{{ item.header }}</p>
+                <p class="text-[12px]">
+                    {{ item.content }}
+                </p>
+            </div>
+        </div>
+    </section>
+</template>
+<script setup lang="ts">
+const data = [
+    {
+        header: "Interaktywne ćwiczenia",
+        content:
+            "Praktyczne zadania pomogą Ci utrwalić wiedzę i rozwinąć umiejętności językowe.",
+    },
+    {
+        header: "Słownik",
+        content: "Rozszerzaj swoje słownictwo dzięki wbudowanemu słownikowi.",
+    },
+    {
+        header: "Gry językowe",
+        content: "Dzięki grom przyswajasz nowo poznane słownictwo.",
+    },
+]
+</script>
