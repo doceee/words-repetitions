fba0eedc4fa45d13bb2b5e55e8aaaa5bad4c2dbe
doceee <piotr96X@wp.pl>
2024-08-26 02:36:47 +0200

    add type

--- backend/prisma/seed/seedUserActivities.ts
+++ backend/prisma/seed/seedUserActivities.ts
@@ -1,4 +1,4 @@
-import { PrismaClient, User, ActivityType } from '@prisma/client';
+import { PrismaClient, User, ActivityType, UserActivity } from '@prisma/client';
 import * as dayjs from 'dayjs';
 import { words } from './seedData';
 
@@ -6,7 +6,7 @@ export const seedUserActivities = async (prisma: PrismaClient, user: User) => {
     const count = Math.floor(Math.random() * (30 - 20 + 1)) + 20;
     let reviewsCount = 0;
 
-    const seedData = [];
+    const seedData: Omit<UserActivity, 'id'>[] = [];
     for (let i = 0; i <= count; i++) {
         const shouldAddLoginActivity = Math.random() < 0.7;
         let date = dayjs().subtract(i, 'd').format();
@@ -15,7 +15,7 @@ export const seedUserActivities = async (prisma: PrismaClient, user: User) => {
             seedData.push({
                 userId: user.id,
                 activity: ActivityType.Login,
-                activity_time: date
+                activity_time: new Date(date)
             });
 
             if (Math.random() < 0.6) {
@@ -23,7 +23,7 @@ export const seedUserActivities = async (prisma: PrismaClient, user: User) => {
                 seedData.push({
                     userId: user.id,
                     activity: ActivityType.Review,
-                    activity_time: date
+                    activity_time: new Date(date)
                 });
             }
         }
