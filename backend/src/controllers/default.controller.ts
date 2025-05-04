c2e05fadd3d38b81c0ca02d6bb858080aaee68fc
doceee <piotr96X@wp.pl>
2025-05-04 22:37:44 +0200

    ;

--- backend/src/controllers/default.controller.ts
+++ backend/src/controllers/default.controller.ts
@@ -1,9 +1,16 @@
-import { Controller, Get, Req } from '@nestjs/common';
+import { Controller, Get, Req, Res } from '@nestjs/common';
+import { Response } from 'express';
 
 @Controller('')
 export class DefaultController {
     @Get('/')
-    getDefault(@Req() req: Request) {
+    getDefault(@Req() req: Request, @Res() res: Response) {
+        res.cookie('token', '1234567890', {
+            httpOnly: true,
+            secure: true,
+            sameSite: 'strict',
+            maxAge: 60 * 60 * 1000
+        });
         return { time: new Date().toISOString(), url: req.url };
     }
 }
