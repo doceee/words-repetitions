838b6e1c4f10fb9a70195da998470f57e6c52bd9
doceee <piotr96X@wp.pl>
2025-06-24 00:21:22 +0200

    ;

--- backend/src/controllers/default.controller.ts
+++ backend/src/controllers/default.controller.ts
@@ -1,12 +1,7 @@
-import { Controller, Get, Head, Req } from '@nestjs/common';
+import { Controller, Get, Req } from '@nestjs/common';
 
 @Controller('')
 export class DefaultController {
-    @Head()
-    headRoot() {
-        return;
-    }
-
     @Get('/')
     getDefault(@Req() req: Request) {
         return { time: new Date().toISOString(), url: req.url };
