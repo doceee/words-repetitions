5cfb2074ae09996f7102aa6e5d86046d15a21d72
doceee <piotr96X@wp.pl>
2025-06-24 12:25:01 +0200

    ;

--- frontend/cypress/support/commands.ts
+++ frontend/cypress/support/commands.ts
@@ -9,36 +9,36 @@ export {}; // Ensure this file is treated as a module
 // commands please read more here:
 // https://on.cypress.io/custom-commands
 // ***********************************************
-Cypress.Commands.add(
-    'login',
-    (
-        email = Cypress.env('adminEmail'),
-        password = Cypress.env('adminPassword')
-    ) => {
-        cy.visit('/login');
+// Cypress.Commands.add(
+//     'login',
+//     (
+//         email = Cypress.env('adminEmail'),
+//         password = Cypress.env('adminPassword')
+//     ) => {
+//         cy.visit('/login');
 
-        cy.get('input[name="email"]').type(email);
+//         cy.get('input[name="email"]').type(email);
 
-        cy.get('input[name="password"]').type(password);
+//         cy.get('input[name="password"]').type(password);
 
-        cy.get('button[type="submit"]').click();
+//         cy.get('button[type="submit"]').click();
 
-        cy.wait(500);
-    }
-);
+//         cy.wait(500);
+//     }
+// );
 
-Cypress.Commands.add('logout', () => {
-    cy.get('[data-testid="logout"]').click();
+// Cypress.Commands.add('logout', () => {
+//     cy.get('[data-testid="logout"]').click();
 
-    cy.wait(500);
-});
+//     cy.wait(500);
+// });
 
-declare global {
-    namespace Cypress {
-        interface Chainable {
-            login(email: string, password: string): Chainable<void>;
-            logout(): Chainable<void>;
-            // Add other custom commands here
-        }
-    }
-}
+// declare global {
+//     namespace Cypress {
+//         interface Chainable {
+//             login(email: string, password: string): Chainable<void>;
+//             logout(): Chainable<void>;
+//             // Add other custom commands here
+//         }
+//     }
+// }
